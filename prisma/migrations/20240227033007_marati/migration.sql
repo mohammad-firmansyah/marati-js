a87bc58b9f9733a8abf5a16f014303673835ba80
mohammad firmansyah <firmansmoh842@gmail.com>
2024-02-27 10:49:46 +0700

    add : scheme for marati

---
+++ prisma/migrations/20240227033007_marati/migration.sql
@@ -0,0 +1,74 @@
+-- CreateEnum
+CREATE TYPE "ComponentType" AS ENUM ('TEXT', 'SWITCH', 'GAUGE', 'LINEGRAPH');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "fullname" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "dashboard" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT,
+    "description" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "dashboard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "component" (
+    "id" TEXT NOT NULL,
+    "type" "ComponentType" NOT NULL,
+    "x" INTEGER NOT NULL,
+    "y" INTEGER NOT NULL,
+    "dashboard_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "topic" TEXT,
+    "rules" JSONB,
+    "model_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "component_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "model" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "input" JSONB NOT NULL,
+    "output" JSONB NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "model_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "dashboard_name_key" ON "dashboard"("name");
+
+-- AddForeignKey
+ALTER TABLE "dashboard" ADD CONSTRAINT "dashboard_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "component" ADD CONSTRAINT "component_dashboard_id_fkey" FOREIGN KEY ("dashboard_id") REFERENCES "dashboard"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "component" ADD CONSTRAINT "component_model_id_fkey" FOREIGN KEY ("model_id") REFERENCES "model"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "model" ADD CONSTRAINT "model_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
