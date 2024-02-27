b1f3118f11b4dd1c90f7e35ca07d8149c137598d
mohammad firmansyah <firmansmoh842@gmail.com>
2024-02-27 12:04:34 +0700

    add : update url of swagger UI

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,10 @@ async function bootstrap() {
   .setTitle("Marati Js Server")
   .setDescription("The marati backend server")
   .setVersion('1.0')
-  .addTag('cats')
   .build()
 
   const document = SwaggerModule.createDocument(app,config)
-  SwaggerModule.setup('api',app,document)
+  SwaggerModule.setup('docs',app,document)
   await app.listen(3000);
 }
 bootstrap();
