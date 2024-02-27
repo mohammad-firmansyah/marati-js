7c641b1b801cdbbf3cece14bf0198627d489e152
mohammad firmansyah <firmansmoh842@gmail.com>
2024-02-27 12:04:50 +0700

    add : dto for dashboard

--- src/dashboard/dto/create-dashboard.dto.ts
+++ src/dashboard/dto/create-dashboard.dto.ts
@@ -1,6 +1,16 @@
 import { IsString } from "class-validator";
 
 export class CreateDashboardDto {
-  
+  @IsString()
+  name : String 
+
+  @IsString()
+  username : String
+
+  @IsString()
+  password: String
+
+  @IsString()
+  description : String
 
 }
