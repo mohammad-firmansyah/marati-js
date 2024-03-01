5aced7a25fc5f2693a72df85c0a04a20cce50e5c
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-01 16:35:19 +0700

    add : guards

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
+import { JwtPayload } from 'src/contracts/jwt-payload/jwt-payload.interface';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService : JwtService,
+    private prismaService : PrismaService){}
+
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    try {
+      
+      const request : Request = context.switchToHttp().getRequest();
+      
+      const token : string = request.headers['authorization'];
+      
+      if (!token) throw new UnauthorizedException()
+      
+      const payload : JwtPayload = await this.jwtService.verifyAsync(
+        token,
+        {secret:process.env.SECRET_KEY}
+      )
+      request.user = payload;
+
+      return true
+    } catch (err :unknown){
+      throw new UnauthorizedException();
+    }
+
+  }
+}
