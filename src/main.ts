c3868a67f01b8ca58ae1202f907faf6f20aeb37a
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-01 16:34:20 +0700

    add : swagger ui for authorization

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,10 +9,12 @@ async function bootstrap() {
   .setTitle("Marati Js Server")
   .setDescription("The marati backend server")
   .setVersion('1.0')
+  .addBearerAuth({ in: 'header', type: 'apiKey', name: 'authorization' })
   .build()
 
   const document = SwaggerModule.createDocument(app,config)
   SwaggerModule.setup('docs',app,document)
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
